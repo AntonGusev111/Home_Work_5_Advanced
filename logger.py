8f12702d9312ac7daf5e70c2c3d535ee0db9d82e
AntonGesev111 <gusevad111@gmail.com>
2022-01-27 20:12:42 +0300

    done

---
+++ logger.py
@@ -0,0 +1,32 @@
+import os
+import csv
+from datetime import datetime
+
+
+def logger(some_func):
+    def inner_func(*args):
+        path = os.path.join(f'{input("input path name: ")}',f'{input("input csv name: ")}.csv')
+        #path = os.path.join(r'logpath\logs_file.csv') #variant 2 for variable 'path'
+        result = some_func(*args)
+        if isinstance(result, dict):
+            result = list(some_func(*args).values())
+            result.insert(0,str(datetime.date(datetime.now())))
+            result.insert(1,str(datetime.now().time())[0:8])
+            result.insert(2, some_func.__name__)
+            writer(path,result)
+        else:
+            writer(path,[str(datetime.date(datetime.now())), str(datetime.now().time())[0:8], result])
+
+        return result
+    return inner_func
+
+
+
+def writer(path,data):
+    if os.path.exists(str(path.split('\\')[0])) == False:
+        os.mkdir(str(path.split('\\')[0]))
+    with open(path,"a",newline="\n") as f:
+      writer = csv.writer(f,delimiter = ',',lineterminator="\r")
+      if os.stat(path).st_size == 0:
+          writer.writerow(['date','time','func_name','city','temp', 'desc'])
+      writer.writerow(data)
---
+++ main.py
@@ -0,0 +1,25 @@
+import requests
+import bs4
+from logger import logger
+
+
+@logger
+def wheather (city):
+    try:
+        result_dict = {'city':'', 'temp': '', 'desc':''}
+        response = requests.get(f'https://sinoptik.com.ru/погода-{city.lower()}')
+        response.raise_for_status()
+        soup = bs4.BeautifulSoup(response.text, features='html.parser')
+        div = soup.find_all('div', class_='weather__content_tab current')
+        for item in div:
+            temp = item.find('b')
+            temp1 = temp.find_next('b')
+            desc = item.find('label', class_='show-tooltip').text
+        result_dict['city'] = city.capitalize()
+        result_dict['temp'] = str(f'{temp.text} {temp1.text}')
+        result_dict['desc'] = desc.replace('\n','')
+        return result_dict
+    except:
+        return ("You'r input wrong sity")
+w = wheather('москва')
+
